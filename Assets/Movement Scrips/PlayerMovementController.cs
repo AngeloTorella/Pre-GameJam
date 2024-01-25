8ad09acdcb5cbcffdaecb0393da09ff4a5b9dd97
APalaciosQ <93203495+APalaciosQ@users.noreply.github.com>
2024-01-25 09:53:55 -0400

    Prueba

---
+++ Assets/Movement Scrips/PlayerMovementController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float playerMoveSpeed;
+    [SerializeField] private float playerJumpForce;
+
+    [SerializeField] private LayerMask grounLayer;
+
+    [SerializeField] private Transform grounSensorT;
+
+    [SerializeField] private float largo = 1.0f;
+    [SerializeField] private float ancho = 1.0f;
+
+    private Rigidbody2D rb2D;
+    private Animator    animator;
+
+    private float       inputX;
+
+    private bool        isGrounded;
+
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.rb2D = GetComponent<Rigidbody2D>();
+
+        this.isGrounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //Actualiza el estado del booleano isGrouded cada frame
+        isGrounded = Physics2D.OverlapBox(grounSensorT.position, new Vector2(largo, ancho), grounLayer);
+        Debug.Log(isGrounded);
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+
+        //Movimiento horizontal
+        rb2D.velocity = new Vector2(inputX * playerMoveSpeed, rb2D.velocity.y);
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2D.AddForce(new Vector2(0f, playerJumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(grounSensorT.position, new Vector2(largo, ancho));
+    }
+}
